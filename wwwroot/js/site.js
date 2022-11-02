088d9983880d20f474305b39e9604fd04f9f7d9f
Luise Katherine Arósquita <luisekatherine@hotmail.com>
2022-11-02 18:25:20 -0300

    Inclusão de carousel js

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,4 +1,35 @@
 ﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
 // for details on configuring this project to bundle and minify static web assets.
 
-// Write your JavaScript code.
+const controls = document.querySelectorAll(".control");
+let currentItem = 0;
+const items = document.querySelectorAll(".item");
+const maxItems = items.length;
+controls.forEach((control) => {
+    control.addEventListener("click", (e) => {
+      isLeft = e.target.classList.contains("arrow-left");
+  
+      if (isLeft) {
+        currentItem -= 1;
+      } else {
+        currentItem += 1;
+      }
+  
+      if (currentItem >= maxItems) {
+        currentItem = 0;
+      }
+  
+      if (currentItem < 0) {
+        currentItem = maxItems - 1;
+      }
+  
+      items.forEach((item) => item.classList.remove("current-item"));
+  
+      items[currentItem].scrollIntoView({
+        behavior: "smooth",
+        inline: "center"
+      });
+  
+      items[currentItem].classList.add("current-item");
+    });
+  });
